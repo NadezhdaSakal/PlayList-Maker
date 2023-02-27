35a01f64e6541877cc38eea1bfa3f11d43978444
NadezhdaSakal <sakaln88@gmail.com>
2023-02-27 23:15:20 +0300

    add data class Track recyclerAdapter $ viewHolder

--- app/src/main/java/com/sakal/playlistmaker/adapters/SearchRecyclerAdapter.kt
+++ app/src/main/java/com/sakal/playlistmaker/adapters/SearchRecyclerAdapter.kt
@@ -1,4 +1,33 @@
 package com.sakal.playlistmaker.adapters
 
-class SearchRecyclerAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.sakal.playlistmaker.R
+import com.sakal.playlistmaker.data.Track
+import com.sakal.viewHolders.SearchViewHolder
+
+class SearchRecyclerAdapter(private val items: ArrayList<Track>, private val context: Context) : RecyclerView.Adapter<SearchViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_search_recycler, parent, false)
+        return SearchViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        holder.bind(items[position])
+        holder.itemView.setOnClickListener {
+            Toast.makeText(
+                context,
+                "${position}: ${items[position].artistName} - ${items[position].trackName}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 }
--- app/src/main/java/com/sakal/viewHolders/SearchViewHolder.kt
+++ app/src/main/java/com/sakal/viewHolders/SearchViewHolder.kt
@@ -1,4 +1,32 @@
 package com.sakal.viewHolders
 
-class SearchViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.sakal.playlistmaker.R
+import com.sakal.playlistmaker.data.Track
+
+class SearchViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+    private val trackName: TextView = itemView.findViewById(R.id.trackName)
+    private val artistName: TextView = itemView.findViewById(R.id.artistNameTrackTime)
+    private val ava: ImageView = itemView.findViewById(R.id.trackAva)
+
+
+    fun bind(track: Track) {
+        trackName.text = track.trackName
+        artistName.text = "${track.artistName} • ${track.trackTime}"
+
+        Glide.with(itemView)
+            .load(track.artworkUrl100)
+            .centerCrop()
+            .placeholder(R.drawable.track_icon)
+            .transform(RoundedCorners(2))
+            .into(ava)
+
+    }
 }
+
