9f68e4ae283e33ad67a597ad008bfb93245c77f0
NadezhdaSakal <sakaln88@gmail.com>
2023-03-10 21:23:52 +0300

    add retrofit add placeholder

--- app/src/main/java/com/sakal/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/sakal/playlistmaker/SearchActivity.kt
@@ -9,57 +9,46 @@ import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.recyclerview.widget.RecyclerView
+import androidx.core.view.isVisible
 import com.sakal.playlistmaker.adapters.SearchRecyclerAdapter
+import com.sakal.playlistmaker.databinding.ActivitySearchBinding
+import com.sakal.playlistmaker.model.ApiConstants
 import com.sakal.playlistmaker.model.Track
+import com.sakal.playlistmaker.model.TrackResponse
+import com.sakal.playlistmaker.model.iTunesSearchAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class SearchActivity : AppCompatActivity() {
 
     companion object {
         const val SEARCH_QUERY = "SEARCH_QUERY"
-        val  DATA_BASE = arrayListOf(
-            Track(
-                "Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Sweet Child O'Mine",
-                " Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg",
-            )
+        private val tracks = ArrayList<Track>()
 
-        )
     }
 
-    private var searchInputQwery = ""
+    private lateinit var tracksAdapter: SearchRecyclerAdapter
+    private lateinit var binding: ActivitySearchBinding
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(ApiConstants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val serviceSearch = retrofit.create(iTunesSearchAPI::class.java)
+
+    private var searchText = ""
     private lateinit var searchInput: EditText
     private lateinit var searchInputClearButton: ImageView
 
     private val searchInputTextWatcher = object : TextWatcher {
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             searchInputClearButton.visibility = clearButtonVisibility(s)
-            searchInputQwery = s.toString()
+            searchText = s.toString()
         }
 
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -75,22 +64,21 @@ class SearchActivity : AppCompatActivity() {
         initSearch()
 
         initRecycler()
-
     }
 
     private fun initToolbar() {
-        findViewById<Toolbar>(R.id.search_toolbar).setNavigationOnClickListener() {
+        findViewById<Toolbar>(R.id.search_toolbar).setNavigationOnClickListener {
             finish()
         }
     }
 
     private fun initSearch() {
 
-        searchInput = findViewById(R.id.input_search_form)
+        searchInput = binding.inputSearchForm
         searchInput.requestFocus()
         searchInput.addTextChangedListener(searchInputTextWatcher)
 
-        searchInputClearButton = findViewById(R.id.clear_form)
+        searchInputClearButton = binding.clearForm
         searchInputClearButton.visibility = clearButtonVisibility(searchInput.text)
         searchInputClearButton.setOnClickListener {
             clearSearchForm()
@@ -118,18 +106,47 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(SEARCH_QUERY, searchInputQwery)
+        outState.putString(SEARCH_QUERY, searchText)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        searchInputQwery = savedInstanceState.getString(SEARCH_QUERY, "")
-        searchInput.setText(searchInputQwery)
+        searchText = savedInstanceState.getString(SEARCH_QUERY, "")
+        searchInput.setText(searchText)
+    }
+
+    private fun searchTrack() {
+        serviceSearch.searchTrack(searchInput.text.toString())
+            .enqueue(object : Callback<TrackResponse> {
+                override fun onResponse(
+                    call: Call<TrackResponse>,
+                    response: Response<TrackResponse>,
+                ) {
+                    if (searchText.isNotEmpty() && !response.body()?.results.isNullOrEmpty() && response.code() == ApiConstants.SUCCESS_CODE) {
+                        tracks.clear()
+                        tracks.addAll(response.body()?.results!!)
+                        tracksAdapter.notifyDataSetChanged()
+                        binding.placeholderNothingWasFound.isVisible = false
+                        binding.placeholderCommunicationsProblem.isVisible = false
+                    } else {
+                        binding.placeholderNothingWasFound.isVisible = true
+                        binding.placeholderCommunicationsProblem.isVisible = false
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    binding.placeholderCommunicationsProblem.isVisible = true
+                    binding.placeholderNothingWasFound.isVisible = false
+                }
+            })
     }
 
     private fun initRecycler() {
-        val searchResultRv = findViewById<RecyclerView>(R.id.recycler_view)
-        searchResultRv.adapter = SearchRecyclerAdapter(DATA_BASE)
+        binding.recyclerView.apply {
+            tracksAdapter = SearchRecyclerAdapter(tracks)
+            //recyclerView.adapter = tracksAdapter
+
+        }
     }
 
 }
