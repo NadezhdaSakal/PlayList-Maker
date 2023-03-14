28c034e8424444274bc2b115a4f2537592640810
NadezhdaSakal <sakaln88@gmail.com>
2023-03-14 19:48:11 +0300

    toolbar

--- app/src/main/java/com/sakal/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/sakal/playlistmaker/SearchActivity.kt
@@ -24,24 +24,21 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
-    //Переменная для работы с вводимым запросом
     var textSearch = ""
-    //Переменные для работы с UI
     lateinit var searchEditText: EditText
     lateinit var searchClearIcon: ImageView
-    lateinit var buttonArrowBackSettings: androidx.appcompat.widget.Toolbar
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
     lateinit var searchAdapter: SearchRecyclerAdapter
     lateinit var placeholderNothingWasFound: LinearLayout
     lateinit var placeholderCommunicationsProblem: LinearLayout
     lateinit var buttonReturn: Button
 
-    //Подключаем Retrofit
     private val retrofit = Retrofit.Builder()
         .baseUrl(ApiConstants.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    private val serviceiTunesSearch = retrofit.create(iTunesSearchAPI::class.java)
+    private val serviceSearch = retrofit.create(iTunesSearchAPI::class.java)
 
     private val tracks = ArrayList<Track>()
 
@@ -49,101 +46,85 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        //RecyclerView
-        setAdapter(tracks)
 
-        //Присвоить значение переменным
+        initRecycler(tracks)
+
         initViews()
 
-        //Listener
         setListeners()
 
-        //Работа с вводимым текстом
+        initToolbar()
+
         inputText()
     }
 
-    //Присвоить значение переменным
-    private fun initViews(){
+    private fun initViews() {
         searchClearIcon = findViewById(R.id.clear_form)
         searchEditText = findViewById(R.id.input_search_form)
         searchEditText.setText(textSearch)
         placeholderNothingWasFound = findViewById(R.id.placeholderNothingWasFound)
         placeholderCommunicationsProblem = findViewById(R.id.placeholderCommunicationsProblem)
         buttonReturn = findViewById(R.id.button_return)
-
-        buttonArrowBackSettings = findViewById(R.id.search_toolbar)
     }
 
-    //Настроить Listeners
-    private fun setListeners(){
-        //Обработка нажатия на ToolBar "<-" и переход
-        // на главный экран через закрытие экрана "Настройки"
-        buttonArrowBackSettings.setOnClickListener(){
-            finish()
-        }
+    private fun setListeners() {
 
         searchClearIcon.setOnClickListener {
-            //Объект для работы с клавиатурой
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-            //Очистить поле для ввода
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             searchEditText.setText("")
-            //Скрыть клавиатуру
             inputMethodManager?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
 
-            //Убрать информацию о неудачных запросах
-            placeholderNothingWasFound.isVisible  = false
             placeholderNothingWasFound.isVisible = false
-            //Очистить найденный список треков
+            placeholderNothingWasFound.isVisible = false
             tracks.clear()
             searchAdapter.notifyDataSetChanged()
         }
 
-        //Поиск трека
         searchEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                searchTrack()
+                getTrack()
                 true
             }
             false
         }
 
-        //Повторить предыдущий запрос после нажатия на кнопку "Обновить"
-        buttonReturn.setOnClickListener(){
+        buttonReturn.setOnClickListener() {
             placeholderCommunicationsProblem.visibility = View.INVISIBLE
-            searchTrack()
+            getTrack()
         }
 
     }
 
-    //Настроить RecyclerView
-    fun setAdapter(tracks : ArrayList<Track>){
+    private fun initToolbar() {
+        toolbar = findViewById(R.id.search_toolbar)
+        toolbar.setNavigationOnClickListener() {
+            finish()
+        }
+    }
+
+    fun initRecycler(tracks: ArrayList<Track>) {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         searchAdapter = SearchRecyclerAdapter(tracks)
         recyclerView.adapter = searchAdapter
     }
 
-    //Работа с вводимым текстом
-    private fun inputText(){
+    private fun inputText() {
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // empty
             }
 
-            //Действие при вводе текста
             override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 searchClearIcon.visibility = searchClearIconVisibility(s)
                 textSearch = searchEditText.getText().toString()
             }
 
             override fun afterTextChanged(s: Editable?) {
-                // empty
             }
         }
-        //Связать поля для ввода и TextWatcher
         searchEditText.addTextChangedListener(simpleTextWatcher)
     }
 
-    //Скрыть или показать кнопку для сброса ввода
     private fun searchClearIconVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
             View.GONE
@@ -152,13 +133,11 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
-    //Cохранить уже имеющееся состояние Activity
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(TEXT_SEARCH,textSearch)
+        outState.putString(TEXT_SEARCH, textSearch)
     }
 
-    //Получить сохранённое значение Activity из onSaveInstanceState
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         textSearch = savedInstanceState.getString(TEXT_SEARCH).toString()
@@ -169,22 +148,20 @@ class SearchActivity : AppCompatActivity() {
         const val TEXT_SEARCH = "TEXT_SEARCH"
     }
 
-    //Поиск трека черезе Retrofit
-    private fun searchTrack(){
-        serviceiTunesSearch.searchTrack(searchEditText.text.toString())
-            .enqueue(object : Callback<TrackResponse>{
+    private fun getTrack() {
+        serviceSearch.searchTrack(searchEditText.text.toString())
+            .enqueue(object : Callback<TrackResponse> {
                 override fun onResponse(
                     call: Call<TrackResponse>,
                     response: Response<TrackResponse>,
                 ) {
-                    if (textSearch.isNotEmpty() && !response.body()?.results.isNullOrEmpty() && response.code() == 200){
+                    if (textSearch.isNotEmpty() && !response.body()?.results.isNullOrEmpty() && response.code() == ApiConstants.SUCCESS_CODE) {
                         tracks.clear()
                         tracks.addAll(response.body()?.results!!)
                         searchAdapter.notifyDataSetChanged()
                         placeholderNothingWasFound.isVisible = false
                         placeholderCommunicationsProblem.isVisible = false
-                    }
-                    else{
+                    } else {
                         placeholderNothingWasFound.isVisible = true
                         placeholderCommunicationsProblem.isVisible = false
                     }
