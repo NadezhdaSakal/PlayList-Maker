4ccf7dd5c6b29633944e4547796d5defe4f3bf96
NadezhdaSakal <sakaln88@gmail.com>
2023-06-24 02:47:32 +0300

    override LibraryActivity

--- app/src/main/java/com/sakal/playlistmaker/media_library/ui/activity/LibraryActivity.kt
+++ app/src/main/java/com/sakal/playlistmaker/media_library/ui/activity/LibraryActivity.kt
@@ -2,11 +2,46 @@ package com.sakal.playlistmaker.media_library.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import com.sakal.playlistmaker.databinding.ActivityLibraryBinding
+import com.sakal.playlistmaker.media_library.ui.MediaRouter
+import com.sakal.playlistmaker.media_library.ui.adapters.ViewPagerAdapter
 import com.sakal.playlistmaker.R
 
 class LibraryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLibraryBinding
+    private lateinit var tabMediator: TabLayoutMediator
+    private val navigationRouter = MediaRouter(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_library)
+        binding = ActivityLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) {
+            tab, position ->
+            when(position) {
+                0 -> tab.text = resources.getString(R.string.favorites_tracks)
+                else -> tab.text = resources.getString(R.string.playlists)
+            }
+        }
+
+        tabMediator.attach()
+
+        initToolbar()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
+    }
+
+    private fun initToolbar() {
+        binding.mediatekaToolbar.setNavigationOnClickListener() {
+            navigationRouter.goBack()
+        }
     }
 }
