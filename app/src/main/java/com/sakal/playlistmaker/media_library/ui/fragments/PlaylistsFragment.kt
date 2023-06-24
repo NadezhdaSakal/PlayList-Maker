c1bd49a90cf97c086de5dea41f0ffd991e21154f
NadezhdaSakal <sakaln88@gmail.com>
2023-06-24 20:50:11 +0300

    fix fragments

--- app/src/main/java/com/sakal/playlistmaker/media_library/ui/fragments/PlaylistsFragment.kt
+++ app/src/main/java/com/sakal/playlistmaker/media_library/ui/fragments/PlaylistsFragment.kt
@@ -12,14 +12,21 @@ import com.sakal.playlistmaker.media_library.ui.viewmodels.PlaylistsFragmentView
 class PlaylistsFragment : Fragment() {
 
     private val viewModel by viewModel<PlaylistsFragmentViewModel>()
+    private var _binding: FragmentPlaylistsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        val binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+        _binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
